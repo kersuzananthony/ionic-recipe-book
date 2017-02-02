6aee841a849b4dce776cb889cfa275a12d6576fe
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-02 13:23:33 +0100

    Configures root page of the app

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { RecipesPage } from "../recipes/recipes";
 
-/*
-  Generated class for the Tabs page.
+import { ShoppingListPage } from "../shopping-list/shopping-list";
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
-
+  shoppingListPage = ShoppingListPage;
+  recipesPage = RecipesPage;
 }
