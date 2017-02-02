e5bb36d8faf15825f1545f18586ab02e6584f1ba
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-02 16:35:00 +0100

    Adds Recipe Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipePage } from "../pages/recipe/recipe";
 import { EditRecipePage } from "../pages/edit-recipe/edit-recipe";
 
 import { ShoppingListService } from "../services/shopping.service";
+import { RecipeService } from "../services/recipe.service";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { ShoppingListService } from "../services/shopping.service";
   ],
   providers: [
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ShoppingListService
+    ShoppingListService,
+	  RecipeService
   ]
 })
 export class AppModule {}
---
+++ src/services/recipe.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Recipe } from "../models/recipe";
+
+@Injectable()
+export class RecipeService {
+	private recipes: Recipe[];
+
+	getRecipes(): Recipe[] {
+		return this.recipes.slice();
+	}
+
+	addRecipe(recipe: Recipe) {
+		this.recipes.push(recipe);
+	}
+
+	updateRecipe(index: number, newRecipe: Recipe) {
+		this.recipes[index] = newRecipe;
+	}
+
+	removeRecipe(index: number) {
+		this.recipes.splice(index, 1);
+	}
+}
