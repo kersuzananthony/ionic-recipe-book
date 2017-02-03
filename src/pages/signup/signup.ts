6fcdea695ee28a927788b31bfb2e887f57711886
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-03 10:28:22 +0100

    Adds loadingController and alertController in sign up process

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
+import { LoadingController, Loading, AlertController } from "ionic-angular";
 
 /*
   Generated class for the Signup page.
@@ -15,14 +16,53 @@ import { AuthService } from "../../services/auth.service";
 })
 export class SignupPage {
 
-	constructor(private authService: AuthService) {}
+	loading: Loading;
+
+	constructor(
+		private authService: AuthService,
+	  private loadingCtrl: LoadingController,
+	  private alertCtrl: AlertController
+	) {}
 
   onSubmit(form: NgForm) {
-  	const formValue = form.value;
+		this.displayLoading();
 
+  	const formValue = form.value;
   	this.authService.signup(formValue.email, formValue.password)
-		  .then(data => console.log(data))
-		  .catch(error => console.log('Signup failed!', error));
+		  .then(data => {
+			  this.loading.dismiss();
+		  	console.log(data)
+		  })
+		  .catch(error => {
+		  	this.loading.dismiss();
+		  	this.displayError(error.message || 'Cannot sign up!');
+		  	console.log('Signup failed!', error)
+		  });
   }
 
+  private displayLoading() {
+	  this.loading = this.loadingCtrl.create({
+			content: 'Signing you up...'
+		});
+
+		this.loading.present();
+  }
+
+  private displayError(message: string) {
+		const alert = this.alertCtrl.create({
+			title: 'Sign up Error',
+			message: message,
+			buttons: [
+				{
+					text: 'OK',
+					role: 'cancel',
+					handler: () => {
+						console.log('Alert dismissed!')
+					}
+				}
+			]
+		});
+
+		alert.present();
+  }
 }
